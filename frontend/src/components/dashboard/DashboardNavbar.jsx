3b8ca61099d78ba76ee25734241aaed93459b5be
hiyavasani <hiyavasani.it23d1@scet.ac.in>
2026-06-29 11:21:15 +0530

    frontend

--- frontend/src/components/dashboard/DashboardNavbar.jsx
+++ frontend/src/components/dashboard/DashboardNavbar.jsx
@@ -19,47 +19,46 @@ items-center
 mb-8
 gap-5
 ">
-    <div>
-        <h1
-        className="
-        text-3xl
-        font-bold
-        ">
-        Welcome back, {user?.name || "Director"}
-        </h1>
-        <p className="text-gray-500">
-        Your fleet performance overview
-        </p>
-    </div>
-    <div className="
-    flex
-    items-center
-    gap-4
-    ">
-        <input
-        placeholder="Search cars, bookings..."
-        className="
-        hidden
-        md:block
-        border
-        rounded-xl
-        px-5
-        py-3
-        "/>
-        <button
-        onClick={logout}
-        className="
-        bg-red-500
-        text-white
-        px-5
-        py-3
-        rounded-xl
-        hover:bg-red-600
-        "
-        >
-        Logout
-        </button>
-    </div>
+<div>
+<h1
+className="
+text-3xl
+font-bold
+">
+Welcome back, {user?.name || "Director"}
+</h1>
+<p className="text-gray-500">
+Your fleet performance overview
+</p>
+</div>
+<div className="
+flex
+items-center
+gap-4
+">
+<input
+placeholder="Search cars, bookings..."
+className="
+hidden
+md:block
+border
+rounded-xl
+px-5
+py-3
+"/>
+<button
+onClick={logout}
+className="
+bg-red-500
+text-white
+px-5
+py-3
+rounded-xl
+hover:bg-red-600
+">
+Logout
+</button>
+</div>
 </div>
 )}
 export default DashboardNavbar;
