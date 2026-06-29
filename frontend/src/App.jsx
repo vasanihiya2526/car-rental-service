81cdfea2e2dcbfdae2bc999bb342776c76f1bc16
hiyavasani <hiyavasani.it23d1@scet.ac.in>
2026-06-29 11:04:06 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,61 @@
 import {Routes,Route} from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Cars from "./pages/Cars";
 import CarDetails from "./pages/CarDetails";
-import Login from "./pages/Login";
 import Booking from "./pages/Booking";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Booking from "./pages/UserDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
+import Dashboard from "./pages/Dashboard";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App(){
 return(
 <>
 <Navbar/>
 <Routes>
-<Route path="/" element={<Home/>}/>
-<Route 
-path="/cars" 
-element={<Cars/>}
-/>
-<Route
-path="/car/:id"
-element={<CarDetails/>}
-/>
-<Route
-path="/login"
-element={<Login/>}
-/>
-<Route
-path="/register"
-element={<Register/>}
-/>
-<Route
-path="/user-dashboard"
-element={<UserDashboard/>}
-/>
-<Route
-path="/admin-dashboard"
-element={<AdminDashboard/>}
-/>
+    <Route 
+    path="/"
+    element={<Home/>}
+    />
+    <Route
+    path="/cars"
+    element={<Cars/>}
+    />
+    <Route
+    path="/car/:id"
+    element={<CarDetails/>}
+    />
+    <Route
+    path="/booking/:id"
+    element={
+    <ProtectedRoute>
+    <Booking/>
+    </ProtectedRoute>
+    }
+    />
+    <Route
+    path="/login"
+    element={<Login/>}
+    />
+    <Route
+    path="/register"
+    element={<Register/>}
+    />
+    <Route
+    path="/dashboard"
+    element={
+    <ProtectedRoute>
+    <Dashboard/>
+    </ProtectedRoute>
+    }
+    />
 </Routes>
 <Footer/>
 </>
-)
-}
+)}
 export default App;
